b3686b95aa57e7d7e45c0aa9848122be3d86ee37
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-17 20:57:45 +0000

    initial code implementation of light sensor

--- main/src/sensors/bh1750_sensor.cpp
+++ main/src/sensors/bh1750_sensor.cpp
@@ -1,1 +1,18 @@
+#include "bh1750_sensor.h"
+#include <Wire.h>
+#include <BH1750.h>
 
+
+static BH1750 lightMeter;
+
+bool BH1750Sensor::begin()
+{
+    Wire.begin();
+
+    return lightMeter.begin();
+}
+
+float BH1750Sensor::readLux()
+{
+    return lightMeter.readLightLevel();
+}
