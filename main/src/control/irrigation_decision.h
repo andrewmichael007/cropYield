6fed30494b645fa19c7e89e3d0a06052759835be
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-19 22:05:42 +0000

    this file indicates a decision whether to irrigate or not

---
+++ main/src/control/irrigation_decision.h
@@ -0,0 +1,14 @@
+// this determines what the irrigation can do
+
+// basically it can irrigate, or not irrigate.
+
+#ifndef IRRIGATION_DECISION_H
+#define IRRIGATION_DECISION_H
+
+enum class IrrigationDecision
+{
+    IRRIGATE,
+    DO_NOT_IRRIGATE
+};
+
+#endif
