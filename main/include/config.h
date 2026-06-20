ca841bdd3beb4e4b8946925367187407586966fa
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-20 23:51:03 +0000

    this holds the time configurations for the system

---
+++ main/include/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Irrigation Durations
+
+constexpr unsigned long
+CRITICAL_IRRIGATION_DURATION_MS = 30000;
+
+constexpr unsigned long
+CONTEXT_IRRIGATION_DURATION_MS = 10000;
+
+
+// Settling Period
+
+constexpr unsigned long
+SETTLING_PERIOD_MS = 60000;
+
+#endif
