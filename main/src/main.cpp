cb33593d5d59644f99fc9225fc1700cb5dcd5e07
Andy-Techy <adjeimitchel03@gmail.com>
2026-06-26 13:24:21 +0000

    implemented the state machine using the switch statement

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,52 +1,197 @@
-// the main.cpp is the PlatformIO template. It contains the the system architecture.
-
-// this is the test code for humidity and temperature sensor
-
-//header files
+// original main.cpp
 #include <Arduino.h>
-#include <DHT.h>
+#include "sensors/sensor_manager.h"
 
-//define pin and dht type
-#define DHT_PIN 4
-#define DHT_TYPE DHT22
+#include "control/irrigation_controller.h"
+#include "control/irrigation_profile.h"
 
-//call the dht function
-DHT dht(DHT_PIN, DHT_TYPE);
+#include "control/relay_controller.h"
+
+#include "system/system_controller.h"
+
+#include "../include/config.h"
+
+
+//creating objects
+//... later these methods would be called on these objects
+SensorManager sensorManager;
+
+IrrigationController irrigationController;
+
+RelayController relayController;
+
+SystemController systemController;
+
+IrrigationProfile currentProfile;
 
 
 void setup()
 {
     Serial.begin(115200);
-    dht.begin();
 
-    Serial.println("DHT22 Test Starting...");
+    //call the begin function on sensorManager object
+    sensorManager.begin();
+
+    //call the begin function on relayController object
+    relayController.begin();
 }
 
 void loop()
 {
-    // defining temperature and humidity as float 
-    float temperature = dht.readTemperature();
-    float humidity = dht.readHumidity();
+    //switch : main state, we use .getState() function to get state
+    switch
+    (
+        systemController.getState()
+    )
 
-    // we first check if temperature and humidity readings is not a number
-    if (isnan(temperature) || isnan(humidity))
     {
-        Serial.println("ERROR: failed to read from DHT22");
-        return;
-    }   
-    // if they are numbers, do this...
-    else
-    {
-        Serial.print("Temperature: ");
-        Serial.print(temperature);
-        Serial.print(" °C    ");
+        //case 1: if the system is in monitoring state
+        case SystemState::MONITORING:
+        {
+            //we get sensor data and store in data
+            SensorData data = sensorManager.getSensorData();
 
-        Serial.print("Humidity: ");
-        Serial.print(humidity);
-        Serial.println(" %");
+            // is data valid ? if no ...
+            if(!data.valid)
+            {   
+                //raise a critical error message
+                Serial.println("[CRITICAL] Sensor Failure" );
+
+                //... and set the system state to fault
+                systemController.setState( SystemState::FAULT);
+
+                break;
+            }
+            
+            // .. else if data is valid: 
+            //we set currentProfile to irrigationController
+            currentProfile = irrigationController.evaluate(data);
+
+            if(currentProfile.shouldIrrigate)
+            {
+                relayController.startIrrigation();
+
+                systemController.setState( SystemState::IRRIGATING );
+            }
+
+            break;
+        }
+
+        //case 2: 
+        case SystemState::IRRIGATING:
+        {
+            if( millis() - systemController.getStateStartTime() >= currentProfile.durationMs )
+            
+            {
+                relayController.stopIrrigation();
+
+                systemController.setState(SystemState::SETTLING);
+            }
+
+            break;
+        }
+
+        //case 3: 
+        case SystemState::SETTLING:
+        {
+            if( millis() - systemController.getStateStartTime() >= SETTLING_PERIOD_MS )
+
+            {
+                systemController.setState( SystemState::MONITORING );
+            }
+
+            break;
+        }
+
+        //case 4
+        case SystemState::FAULT:
+        {
+            relayController.stopIrrigation();
+
+            break;
+        }
     }
-
-    //DHT22 reads data every 2 seconds
-    delay(2000);
 }
+
+
+// temporal main.cpp for testing
+
+// #include <Arduino.h>
+
+// #include "sensors/dht22_sensor.h"
+// #include "sensors/soil_moisture_sensor.h"
+
+// DHTSensor dhtSensor;
+// SoilMoistureSensor SoilMoistureSensor;
+
+// void setup()
+// {
+//     Serial.begin(115200);
+
+//     dhtSensor.begin();
+//     SoilmoistureSensor.begin();
+// }
+
+// void loop()
+// {
+//     Serial.println("==========");
+
+//     Serial.print("Temperature: ");
+//     Serial.println(dhtSensor.readTemperature());
+
+//     Serial.print("Humidity: ");
+//     Serial.println(dhtSensor.readHumidity());
+
+//     Serial.print("Raw Moisture: ");
+//     Serial.println(SoilMoistureSensor.readRaw());
+
+//     Serial.print("Moisture (%): ");
+//     Serial.println(SoilMoistureSensor.readPercentage());
+
+//     delay(2000);
+// }
+
+
+// void loop()
+// {   
+//     // we create an object, data on SensorData class of which we run the getSensorData() method
+//     SensorData data = sensorManager.getSensorData();
+
+//     IrrigationDecision decision = irrigationController.evaluate(data);
+
+//     //checking the validity of the data 
+//     //...if data validity is false
+//     if (!data.valid)
+//     {
+//         Serial.println("[CRITICAL] Sensor Failure");
+
+//         //while data is still invalid, continue continue loggin error message after every second
+//         while(true)
+//         {
+//             delay(1000);
+//         }
+//     }
+
+//     //else print output to serial monitor...
+
+//     Serial.println("==========");
+
+//     Serial.print("Temperature: ");
+//     Serial.println(data.temperature);
+
+//     Serial.print("Humidity: ");
+//     Serial.println(data.humidity);
+
+//     Serial.print("Moisture: ");
+//     Serial.println(data.soilMoisture);
+
+//     Serial.print("Light: ");
+//     Serial.println(data.lightIntensity);
+
+//     // print outputs at every 2s
+//     delay(2000);
+
+// }
+
+
 
